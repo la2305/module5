080314eb36e4a68130a6394b922781c045358739
la2305 <lequangphuoc2305@gmail.com>
2023-08-18 13:42:46 +0700

    up

---
+++ ss1/ex2.js
@@ -0,0 +1,11 @@
+let add = function(x,y){
+    return x+y
+}
+console.log(add(4,5));
+
+let add2 =(x,y) => x+y;
+console.log(4+7);
+
+let names =['la','luu','looo'];
+let lengthName = names.map(element =>element.length);
+console.log(lengthName);
